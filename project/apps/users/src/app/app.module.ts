4b7946b2324811b98334a43111b5de1130d9377b
Artem <vasilenko@maxis-it.ru>
2025-12-16 12:12:00 +0300

    Добавил сервис и контроллер

--- project/apps/users/src/app/app.module.ts
+++ project/apps/users/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthenticationModule } from './authentication/authentication.module';
+import { UserModule } from './user/user.module';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthenticationModule, UserModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}

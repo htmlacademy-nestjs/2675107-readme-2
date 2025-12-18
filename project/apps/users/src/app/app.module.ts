863baf4948291d502c8057e43be570052902ae6c
Artem <vasilenko@maxis-it.ru>
2025-12-18 17:07:44 +0300

    Конфигурация для users

--- project/apps/users/src/app/app.module.ts
+++ project/apps/users/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { UserModule } from './user/user.module';
-
+import { ConfigUsersModule } from "@project/shared/config/users"
 
 @Module({
-  imports: [AuthenticationModule, UserModule],
+  imports: [AuthenticationModule, UserModule, ConfigUsersModule],
   controllers: [],
   providers: [],
 })

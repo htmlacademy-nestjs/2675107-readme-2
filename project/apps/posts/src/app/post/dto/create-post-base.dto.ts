a6d3e7799a8352403fbb27f0cf9eb239e45ec283
Artem <vasilenko@maxis-it.ru>
2026-01-29 12:54:25 +0300

    Add ApiProperty on fields

--- project/apps/posts/src/app/post/dto/create-post-base.dto.ts
+++ project/apps/posts/src/app/post/dto/create-post-base.dto.ts
@@ -2,18 +2,30 @@ import { ApiProperty } from '@nestjs/swagger';
 import { PostType } from '@project/shared/app/types';
 import { IsArray, IsDateString, IsEnum, IsOptional, IsString } from 'class-validator';
 
-export class CreatePostBaseDto  {
+export class CreatePostBaseDto {
+  @ApiProperty({
+    description: 'Type of the post',
+    enum: PostType,
+    example: PostType.TEXT,
+  })
   @IsEnum(PostType)
   type: PostType;
 
+  @ApiProperty({
+    description: 'Optional published date',
+    required: false,
+    type: String,
+    example: '2026-01-29T12:00:00.000Z',
+  })
   @IsOptional()
   @IsDateString()
   publishedAt?: string;
 
   @ApiProperty({
     description: 'Post tags',
+    required: false,
+    type: [String],
     example: ['nestjs', 'backend'],
-    required: false
   })
   @IsOptional()
   @IsArray()

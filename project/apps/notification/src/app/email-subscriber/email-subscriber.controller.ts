734c6b0365e039e7d3a871509f6b1e0ed6246e38
Artem <vasilenko@maxis-it.ru>
2026-02-10 16:01:37 +0300

    Add rabbitmq and mailer

--- project/apps/notification/src/app/email-subscriber/email-subscriber.controller.ts
+++ project/apps/notification/src/app/email-subscriber/email-subscriber.controller.ts
@@ -4,19 +4,22 @@ import { RabbitSubscribe } from '@golevelup/nestjs-rabbitmq';
 import { EmailSubscriberService } from './email-subscriber.service';
 import { CreateSubscriberDto } from './dto/create-subscriber.dto';
 import { RabbitRouting } from '@project/shared/app/types';
+import { MailService } from '../mail/mail.service';
 
 @Controller()
 export class EmailSubscriberController {
   constructor(
     private readonly subscriberService: EmailSubscriberService,
+    private readonly mailService: MailService,
   ) {}
 
   @RabbitSubscribe({
-    exchange: 'readme.notify.income',
+    exchange: 'readme.notify',
     routingKey: RabbitRouting.AddSubscriber,
     queue: 'readme.notify.income',
   })
   public async create(subscriber: CreateSubscriberDto) {
     this.subscriberService.addSubscriber(subscriber);
+    this.mailService.sendNotifyNewSubscriber(subscriber);
   }
 }
